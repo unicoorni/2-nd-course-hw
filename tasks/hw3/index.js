1cecf008a88823290f1a235a263ab67ea7b1d234
uniicoorni <carolinekostina.gmail.com>
2023-09-23 16:05:38 +0300

    hw3

---
+++ tasks/hw3/index.js
@@ -0,0 +1,98 @@
+// task1
+// let password = 'пароль';
+// let userPassword = prompt('Введите пароль');
+
+// if (password === userPassword ) {
+//     alert('Пароль введен верно');
+// } else {
+//     alert('Пароль введен неправильно');
+// }
+
+// task2
+
+// let c = 10;
+// c= -3;
+// c= 2;
+// c= 0;
+
+// if (c>0 && c<10) {
+//     alert('верно');
+// } else {
+//     alert('неверно');
+// }
+
+// task3
+
+// let d =5;
+// let e = 1;
+
+// if (d>100 || e>100) {
+//         alert('верно');
+//     } else {
+//         alert('неверно');
+//     }
+
+
+// task4
+
+// let a = '2';
+// let b = '3';
+
+// alert(Number(a) + Number(b) );
+
+// task5
+
+// let monthNumber = prompt('Введите номер месяца');
+
+// switch (monthNumber) {
+//     case  '1' || '11'|| '12'  :
+//         alert('зима');
+//         break;
+
+//     case  '11'  :
+//         alert('зима');
+//         break;
+
+//     case  '12'  :
+//         alert('зима');
+//         break;    
+
+//     case '3'  : 
+//         alert('Весна');
+//         break;    
+
+//     case '4'  : 
+//         alert('Весна');
+//         break;    
+
+//     case  '5': 
+//         alert('Весна');
+//         break;    
+
+//     case '6': 
+//         alert('лето');
+//         break;   
+//     case  '7' : 
+//         alert('лето');
+//         break;   
+//     case  '8': 
+//         alert('лето');    
+//         break;   
+
+//     case '9' : 
+//         alert('осень');
+//         break;  
+//     case '10' : 
+//         alert('осень');
+//         break;  
+//     case '11' : 
+//         alert('осень');
+//         break;  
+
+//     default:
+//         alert('такого месяца нет');
+//         break;
+// }
+
+// task6
+
